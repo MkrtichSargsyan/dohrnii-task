fe05b913d884d968538354e5acba593cf8b9cbc1
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-12-17 20:11:49 +0400

    Add Footer section

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+import { ReactComponent as TelegramIcon } from '../../assets/telegramIcon.svg';
+import { ReactComponent as FacebookIcon } from '../../assets/facebookIcon.svg';
+import { ReactComponent as TweeterIcon } from '../../assets/twitterIcon.svg';
+import { ReactComponent as InstagramIcon } from '../../assets/instagramIcon.svg';
+import { ReactComponent as TictocIcon } from '../../assets/tiktokIcon.svg';
+
+const FooterContainer = styled.footer`
+  margin: 30px 40px 100px;
+  display: flex;
+  justify-content: space-between;
+`;
 
 function Footer() {
   return (
-    <footer>
-      Footer
-    </footer>
-  )
+    <FooterContainer>
+      <TelegramIcon />
+      <FacebookIcon />
+      <TweeterIcon />
+      <InstagramIcon />
+      <TictocIcon />
+    </FooterContainer>
+  );
 }
 
-export default Footer
+export default Footer;
